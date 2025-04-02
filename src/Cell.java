1d99414bbd229b326450bbd44a8c5d451bdcad7d
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-04-02 13:46:23 -0400

    Added constructors

--- src/Cell.java
+++ src/Cell.java
@@ -11,4 +11,31 @@ public class Cell{
     private int columnIndex;
     private int value;
     private boolean locked;
+
+    /**
+     * Constructor for Cell
+     * @param row the row index of the cell
+     * @param col the column index of the cell
+     * @param value the value of the cell
+     */
+    public Cell(int row, int col, int value){
+        this.rowIndex = row;
+        this.columnIndex = col;
+        this.value = value;
+        this.locked = false;
+    }
+
+    /**
+     * Constructor for Cell
+     * @param row the row index of the cell
+     * @param col the column index of the cell
+     * @param value the value of the cell
+     * @param locked whether the cell is locked or not
+     */
+    public Cell(int row, int col, int value, boolean locked){
+        this.rowIndex = row;
+        this.columnIndex = col;
+        this.value = value;
+        this.locked = locked;
+    }
 }
