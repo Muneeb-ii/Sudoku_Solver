7f4db5b6a9950815ee404fbf7f6b03194fadf8bb
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-04-02 13:44:48 -0400

    Created a Cell class and added fields

---
+++ src/Cell.java
@@ -0,0 +1,14 @@
+/**
+ * Author: Muneeb Azfar Nafees
+ * 
+ * Purpose of class:
+ * 
+ */
+
+public class Cell{
+    
+    private int rowIndex;
+    private int columnIndex;
+    private int value;
+    private boolean locked;
+}
