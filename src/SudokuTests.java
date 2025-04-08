74b1d1d134720e384e2b2f5924bfe64a497af424
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-04-08 15:20:12 -0400

    Added the new code provided

--- src/SudokuTests.java
+++ src/SudokuTests.java
@@ -17,71 +17,32 @@ public class SudokuTests {
         /**
             * Solve Blank Board
         */
-        try {
-            Thread thread = new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                Sudoku game = new Sudoku( 0 , 0 ) ;
-                game.solve();
-                }
-
-            });
-        thread.start();
-
-        boolean result = thread.join(java.time.Duration.ofMillis(10000));
-
-        if (result)
+        Sudoku game = new Sudoku( 0 , 0 ) ;
+        if ( game.solve() ) {
             testScore += 2;
-        } catch (Exception E) {
-            System.out.println( "Exception when solving blank board" );
+        } else {
+            System.out.println( "Blank board not solving" );
         }
 
         /**
         * Solve a Board with a small number of elements
         */
-        try {
-            Thread thread = new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                Sudoku game = new Sudoku( 5 , 0 ) ;
-                game.solve();
-                }
-
-            });
-        thread.start();
-
-        boolean result = thread.join(java.time.Duration.ofMillis(10000));
-
-        if (result)
+        Sudoku game1 = new Sudoku( 5 , 0 ) ;
+        if ( game1.solve() ) {
             testScore += 2;
-        } catch (Exception E) {
-            System.out.println( "Exception when solving blank board" );
+        } else {
+            System.out.println( "5-element board not solving" );
         }
 
         /**
         * Solve a Board with a more elements
         */
-        try {
-            Thread thread = new Thread(new Runnable() {
+        Sudoku game2 = new Sudoku( 40 , 0 ) ;
+        System.out.println( "If your program hangs here, you have an error" );
+        game2.solve() ;
+        System.out.println( "Your program isn't hanging, disregard the note above" );
 
-            @Override
-            public void run() {
-                Sudoku game = new Sudoku( 40 , 0 ) ;
-                game.solve();
-                }
-
-            });
-        thread.start();
-
-        boolean result = thread.join(java.time.Duration.ofMillis(10000));
-
-        if (result)
-            testScore += 2;
-        } catch (Exception E) {
-            System.out.println( "Exception when solving blank board" );
-        }
+        testScore += 2;
 
         return testScore ;
     }
