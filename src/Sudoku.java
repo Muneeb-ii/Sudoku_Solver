ceaa96c024e8e51c7024fcdf271bff3fde6ded3c
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-04-08 07:10:48 -0400

    Added another constructor
    - Takes filename as parameter

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -11,8 +11,24 @@ public class Sudoku {
     private LandscapeDisplay display;
     private int delay;
 
+    /**
+     * Constructor for Sudoku with a random board
+     * @param numLocked the number of locked cells
+     * @param delay the delay in milliseconds
+     */
     public Sudoku(int numLocked, int delay){
         board = new Board(numLocked);
+        this.delay = delay;
+        display = new LandscapeDisplay(board);
+    }
+
+    /**
+     * Constructor for Sudoku using a text file
+     * @param filename the name of the file containing the board
+     * @param delay the delay in milliseconds
+     */
+    public Sudoku(String filename, int delay){
+        board = new Board(filename);
         this.delay = delay;
         display = new LandscapeDisplay(board);
     }
