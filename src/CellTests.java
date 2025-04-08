a340d04e505510caaff1fabdb7f97f96bece9624
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-04-08 04:57:59 -0400

    Added some more tests to check all methods

--- src/CellTests.java
+++ src/CellTests.java
@@ -14,25 +14,76 @@ public class CellTests {
 
         // Test the constructor
         Cell cell = new Cell(1, 2, 3);
-        if (cell.getRow() == 1 && cell.getCol() == 2 && cell.getValue() == 3) {
+        if (cell != null) {
             testScore += 1.0;
+        }
+        else {
+            System.out.println("Constructor test failed");
         }
 
         // Test the locked property
         if (!cell.isLocked()) {
             testScore += 1.0;
         }
+        else {
+            System.out.println("Locked property test failed");
+        }
 
-        // Test the locked property with a locked cell
-        Cell lockedCell = new Cell(1, 2, 3, true);
-        if (lockedCell.isLocked()) {
+        // Test the getRow method
+        if (cell.getRow() == 1) {
             testScore += 1.0;
+        }
+        else {
+            System.out.println("getRow test failed");
+        }
+
+        // Test the getCol method
+        if (cell.getCol() == 2) {
+            testScore += 1.0;
+        }
+        else {
+            System.out.println("getCol test failed");
+        }
+
+        // Test the getValue method
+        if (cell.getValue() == 3) {
+            testScore += 1.0;
+        }
+        else {
+            System.out.println("getValue test failed");
+        }
+
+        // Test the setValue method
+        cell.setValue(5);
+        if (cell.getValue() == 5) {
+            testScore += 1.0;
+        }
+        else {
+            System.out.println("setValue test failed");
+        }
+
+        // Test the setLocked method
+        cell.setLocked(true);
+        if (cell.isLocked()) {
+            testScore += 1.0;
+        }
+        else {
+            System.out.println("setLocked test failed");
+        }
+
+        // Test the toString method
+        String expectedString = "Cell [rowIndex=1, columnIndex=2, value=5, locked=true]";
+        if (cell.toString().equals(expectedString)) {
+            testScore += 1.0;
+        }
+        else {
+            System.out.println("toString test failed");
         }
 
         return testScore;
     }
 
     public static void main(String[] args) {
-        System.out.println(cellTests() + "/3");
+        System.out.println(cellTests() + "/8.0");
     }
 }
