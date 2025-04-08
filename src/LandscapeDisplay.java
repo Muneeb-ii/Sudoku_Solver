01e978176152962f9481e92363dca37d508bd47d
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-04-08 15:20:30 -0400

    Updated as per the code provided

--- src/LandscapeDisplay.java
+++ src/LandscapeDisplay.java
@@ -18,7 +18,6 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
