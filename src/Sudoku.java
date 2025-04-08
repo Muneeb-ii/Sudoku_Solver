f999da121d2a31fd4bcb0e97a5d427f7f1a07dba
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-04-08 08:01:44 -0400

    Added findNextCell and solve methods

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -48,5 +48,62 @@ public class Sudoku {
         }
         return 0;
     }
+
+    /**
+     * Finds the next cell to fill in the Sudoku board
+     * @return the cell with the next value, or null if no valid value is found
+     */
+    public Cell findNextCell() {
+        // Iterate over all cells in row-column order
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board.value(i, j) == 0) { // found an empty cell
+                    int newValue = findNextValue(board.get(j, j));
+                    if (newValue != 0) {
+                        board.set(i, j, newValue);
+                        return board.get(i, j);
+                    } 
+                    else {
+                        return null; // No valid value found for this cell
+                    }
+                }
+            }
+        }
+        return null; // No empty cells found
+    }
+    
+    /**
+     * Solves the Sudoku puzzle using backtracking
+     * @return true if the puzzle is solved, false otherwise
+     */
+    public boolean solve(){
+        LinkedList<Cell> stack = new LinkedList<Cell>();
+        int unspecifiedCells = (9*9) - board.numLocked();
+
+        while (stack.size()<unspecifiedCells){
+            Cell next = findNextCell();
+
+            while (next==null && stack.size()>0){
+                Cell previous = stack.pop();
+                // Update the cell's value by trying the next candidate
+                int newValue = findNextValue(previous);
+                // Update the cell accordingly
+                previous.setValue(newValue);
+                if (previous.getValue()!=0){
+                    next = previous;
+                }
+
+            }
+
+            if(next==null){
+                return false;
+            }
+            else {
+                stack.push(next);
+            }
+        }
+
+        return true;
+    }
 }
 
