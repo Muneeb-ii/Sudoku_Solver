765701c8bb58d0153b18a1b05969248a3e1e0d83
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-04-08 07:03:17 -0400

    Added SodukoTests class

---
+++ src/SudokuTests.java
@@ -0,0 +1,93 @@
+/*
+file name:      SudokuTests.java
+Authors:        Max Bender and Ike Lage
+last modified:  04/01/2025
+
+How to run:     java -ea SudokuTests
+*/
+
+public class SudokuTests {
+
+    /**
+    * Stack Tests
+    */
+    public static double sudokuTests() {
+
+        double testScore = 0. ;
+        /**
+            * Solve Blank Board
+        */
+        try {
+            Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                Sudoku game = new Sudoku( 0 , 0 ) ;
+                game.solve();
+                }
+
+            });
+        thread.start();
+
+        boolean result = thread.join(java.time.Duration.ofMillis(10000));
+
+        if (result)
+            testScore += 2;
+        } catch (Exception E) {
+            System.out.println( "Exception when solving blank board" );
+        }
+
+        /**
+        * Solve a Board with a small number of elements
+        */
+        try {
+            Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                Sudoku game = new Sudoku( 5 , 0 ) ;
+                game.solve();
+                }
+
+            });
+        thread.start();
+
+        boolean result = thread.join(java.time.Duration.ofMillis(10000));
+
+        if (result)
+            testScore += 2;
+        } catch (Exception E) {
+            System.out.println( "Exception when solving blank board" );
+        }
+
+        /**
+        * Solve a Board with a more elements
+        */
+        try {
+            Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                Sudoku game = new Sudoku( 40 , 0 ) ;
+                game.solve();
+                }
+
+            });
+        thread.start();
+
+        boolean result = thread.join(java.time.Duration.ofMillis(10000));
+
+        if (result)
+            testScore += 2;
+        } catch (Exception E) {
+            System.out.println( "Exception when solving blank board" );
+        }
+
+        return testScore ;
+    }
+
+    public static void main(String[] args) {
+        System.out.println( sudokuTests() + "/6" );
+    }
+
+}
