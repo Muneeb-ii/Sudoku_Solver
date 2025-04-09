8022b65c7f41d772573c7a78dc9cb699f3c593d4
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-04-08 20:23:03 -0400

    Added tests for solve2 method

--- src/SudokuTests.java
+++ src/SudokuTests.java
@@ -1,7 +1,8 @@
 /*
 file name:      SudokuTests.java
-Authors:        Max Bender and Ike Lage
-last modified:  04/01/2025
+Authors:        Max Bender and Ike Lage 
+modified:       Muneeb Azfar Nafees
+last modified:  04/08/2025
 
 How to run:     java -ea SudokuTests
 */
@@ -21,7 +22,7 @@ public class SudokuTests {
         if ( game.solve() ) {
             testScore += 2;
         } else {
-            System.out.println( "Blank board not solving" );
+            System.out.println( "Blank board not solving using brute force (solve)" );
         }
 
         /**
@@ -31,7 +32,7 @@ public class SudokuTests {
         if ( game1.solve() ) {
             testScore += 2;
         } else {
-            System.out.println( "5-element board not solving" );
+            System.out.println( "5-element board not solving using brute force (solve)" );
         }
 
         /**
@@ -44,11 +45,38 @@ public class SudokuTests {
 
         testScore += 2;
 
+        Sudoku game3 = new Sudoku( 0 , 0 ) ;
+        if ( game3.solve2() ) {
+            testScore += 2;
+        } else {
+            System.out.println( "Blank board not solving using MRV heuristic (solve2)" );
+        }
+
+        /**
+        * Solve a Board with a small number of elements
+        */
+        Sudoku game4 = new Sudoku( 5 , 0 ) ;
+        if ( game4.solve2() ) {
+            testScore += 2;
+        } else {
+            System.out.println( "5-element board not solving MRV heuristic (solve2)" );
+        }
+
+        /**
+        * Solve a Board with a more elements
+        */
+        Sudoku game5 = new Sudoku( 40 , 0 ) ;
+        System.out.println( "If your program hangs here, you have an error" );
+        game5.solve2() ;
+        System.out.println( "Your program isn't hanging, disregard the note above" );
+
+        testScore += 2;
+
         return testScore ;
     }
 
     public static void main(String[] args) {
-        System.out.println( sudokuTests() + "/6" );
+        System.out.println( sudokuTests() + "/12" );
     }
 
 }
