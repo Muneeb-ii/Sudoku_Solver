1f7eeac99505e47bf1fe035ed71ff97fef77b2aa
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-04-12 15:28:09 -0400

    Add assert statements

--- extension/SudokuTests.java
+++ extension/SudokuTests.java
@@ -15,61 +15,55 @@ public class SudokuTests {
     public static double sudokuTests() {
 
         double testScore = 0. ;
+
         /**
-            * Solve Blank Board
+        * Solve Blank Board using brute force
         */
         Sudoku game = new Sudoku( 0 , 0 ) ;
-        if ( game.solve() ) {
-            testScore += 2;
-        } else {
-            System.out.println( "Blank board not solving using brute force (solve)" );
-        }
-
-        /**
-        * Solve a Board with a small number of elements
-        */
-        Sudoku game1 = new Sudoku( 5 , 0 ) ;
-        if ( game1.solve() ) {
-            testScore += 2;
-        } else {
-            System.out.println( "5-element board not solving using brute force (solve)" );
-        }
-
-        /**
-        * Solve a Board with a more elements
-        */
-        Sudoku game2 = new Sudoku( 40 , 0 ) ;
-        System.out.println( "If your program hangs here, you have an error" );
-        game2.solve() ;
-        System.out.println( "Your program isn't hanging, disregard the note above" );
-
+        assert game.solve() == true : "Blank board not solving using brute force (solve)" ;
         testScore += 2;
 
-        Sudoku game3 = new Sudoku( 0 , 0 ) ;
-        if ( game3.solve2() ) {
-            testScore += 2;
-        } else {
-            System.out.println( "Blank board not solving using MRV heuristic (solve2)" );
-        }
+        /**
+        * Solve a Board with a small number of elements using brute force
+        */
+        Sudoku game1 = new Sudoku( 5 , 0 ) ;
+        assert game1.solve() == true : "5-element board not solving brute force (solve)" ;
+        testScore += 2;
 
         /**
-        * Solve a Board with a small number of elements
+        * Solve a Board with a more elements using brute force
+        */
+        System.out.println( "If your program hangs here, you have an error with the board auxilary constructor");
+        Sudoku game2 = new Sudoku( 40 , 0 ) ;
+        System.out.println( "Your program isn't hanging, disregard the note above" );
+        System.out.println( "If your program hangs here, you have an error within the solve method" );
+        game2.solve() ;
+        System.out.println( "Your program isn't hanging, disregard the note above" );
+        testScore += 2;
+
+        /**
+        * Solve Blank Board using MRV heuristic
+        */
+        Sudoku game3 = new Sudoku( 0 , 0 ) ;
+        assert game3.solve2() == true : "Blank board not solving using MRV heuristic (solve2)" ;
+        testScore += 2;
+
+        /**
+        * Solve a Board with a small number of elements using MRV heuristic
         */
         Sudoku game4 = new Sudoku( 5 , 0 ) ;
-        if ( game4.solve2() ) {
-            testScore += 2;
-        } else {
-            System.out.println( "5-element board not solving MRV heuristic (solve2)" );
-        }
+        assert game4.solve2() == true : "5-element board not solving using MRV heuristic (solve2)" ;
+        testScore += 2;
 
         /**
-        * Solve a Board with a more elements
+        * Solve a Board with a more elements using MRV heuristic
         */
+        System.out.println( "If your program hangs here, you have an error with the board auxilary constructor");
         Sudoku game5 = new Sudoku( 40 , 0 ) ;
+        System.out.println( "Your program isn't hanging, disregard the note above" );
         System.out.println( "If your program hangs here, you have an error" );
         game5.solve2() ;
         System.out.println( "Your program isn't hanging, disregard the note above" );
-
         testScore += 2;
 
         return testScore ;
