c32121a78e785f13086bbbdc7805aad0c25c209a
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-04-12 15:08:27 -0400

    Improve test methods

--- extension/CellTests.java
+++ extension/CellTests.java
@@ -17,56 +17,40 @@ public class CellTests {
         // Test the constructor
         Cell cell = new Cell(1, 2, 3);
         Cell cell2 = new Cell(4, 5, 6, true);
-        if (cell != null && cell2 != null) {
-            testScore += 1.0;
-        }
         assert cell != null && cell2 != null : "Constructor test failed";
+        testScore += 1.0;
 
         // Test the locked property
-        if (!cell.isLocked() && cell2.isLocked()) {
-            testScore += 1.0;
-        }
         assert !cell.isLocked() && cell2.isLocked() : "Locked property test failed";
+        testScore += 1.0;
 
         // Test the getRow method
-        if (cell.getRow() == 1 && cell2.getRow() == 4) {
-            testScore += 1.0;
-        }
         assert cell.getRow() == 1 && cell2.getRow() == 4 : "getRow test failed";
+        testScore += 1.0;
 
         // Test the getCol method
-        if (cell.getCol() == 2 && cell2.getCol() == 5) {
-            testScore += 1.0;
-        }
         assert cell.getCol() == 2 && cell2.getCol() == 5 : "getCol test failed";
+        testScore += 1.0;
 
         // Test the getValue method
-        if (cell.getValue() == 3 && cell2.getValue() == 6) {
-            testScore += 1.0;
-        }
         assert cell.getValue() == 3 && cell2.getValue() == 6 : "getValue test failed";
+        testScore += 1.0;
 
         // Test the setValue method
         cell.setValue(5);
-        if (cell.getValue() == 5) {
-            testScore += 1.0;
-        }
         assert cell.getValue() == 5 : "setValue test failed";
+        testScore += 1.0;
 
         // Test the setLocked method
         cell.setLocked(true);
         cell2.setValue(2); // Attempt to set value when locked
-        if (cell.isLocked() && cell2.getValue() == 6) {
-            testScore += 1.0;
-        }
         assert cell.isLocked() && cell2.getValue() == 6 : "setLocked test failed";
+        testScore += 1.0;
 
         // Test the toString method
         String expectedString = "Cell [rowIndex=1, columnIndex=2, value=5, locked=true]";
-        if (cell.toString().equals(expectedString)) {
-            testScore += 1.0;
-        }
         assert cell.toString().equals(expectedString) : "toString test failed";
+        testScore += 1.0;
 
         return testScore;
     }
